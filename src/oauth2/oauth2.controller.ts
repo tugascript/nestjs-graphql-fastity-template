1df5c0575613c16f3d6c68461e41477f9a4c4e7d
Afonso Barracha <barracha.afonso@gmail.com>
2023-04-03 21:07:20 +0100

    chore(oauth2): add redirect urls

--- src/oauth2/oauth2.controller.ts
+++ src/oauth2/oauth2.controller.ts
@@ -1,7 +1,62 @@
-import { Controller } from '@nestjs/common';
-import { Oauth2Service } from './oauth2.service';
+/*
+ Free and Open Source - GNU GPLv3
 
-@Controller('oauth2')
+ This file is part of nestjs-graphql-fastify-template
+
+ nestjs-graphql-fastify-template is distributed in the
+ hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
+
+ Copyright © 2023
+ Afonso Barracha
+*/
+
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Oauth2Service } from './oauth2.service';
+import { OAuthFlagGuard } from './guards/oauth-flag.guard';
+import { OAuthProvidersEnum } from './enums/oauth-providers.enum';
+import { Public } from '../auth/decorators/public.decorator';
+import { FastifyReply } from 'fastify';
+
+@Controller('api/oauth2')
 export class Oauth2Controller {
   constructor(private readonly oauth2Service: Oauth2Service) {}
+
+  @Public()
+  @UseGuards(OAuthFlagGuard(OAuthProvidersEnum.MICROSOFT))
+  @Get('microsoft')
+  public async microsoft(@Res() res: FastifyReply) {
+    return res.redirect(
+      this.oauth2Service.generateAuthorizationUrl(OAuthProvidersEnum.MICROSOFT),
+    );
+  }
+
+  @Public()
+  @UseGuards(OAuthFlagGuard(OAuthProvidersEnum.GOOGLE))
+  @Get('google')
+  public async google(@Res() res: FastifyReply) {
+    return res.redirect(
+      this.oauth2Service.generateAuthorizationUrl(OAuthProvidersEnum.GOOGLE),
+    );
+  }
+
+  @Public()
+  @UseGuards(OAuthFlagGuard(OAuthProvidersEnum.FACEBOOK))
+  @Get('facebook')
+  public async facebook(@Res() res: FastifyReply) {
+    return res.redirect(
+      this.oauth2Service.generateAuthorizationUrl(OAuthProvidersEnum.FACEBOOK),
+    );
+  }
+
+  @Public()
+  @UseGuards(OAuthFlagGuard(OAuthProvidersEnum.GITHUB))
+  @Get('github')
+  public async github(@Res() res: FastifyReply) {
+    return res.redirect(
+      this.oauth2Service.generateAuthorizationUrl(OAuthProvidersEnum.GITHUB),
+    );
+  }
 }
